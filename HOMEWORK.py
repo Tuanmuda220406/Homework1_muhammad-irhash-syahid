163509217f39079e514f22e14dc9de0a482e5e61
Tuanmuda220406 <muhammad.irhas108@gmail.com>
2024-10-17 16:20:38 +0700

    first commit

---
+++ HOMEWORK.py
@@ -0,0 +1,46 @@
+# RENTAL MOBIL
+# 
+
+
+print("============ RENTAL MOBIL ===========")
+print("TEMPAT RENTAL MOBIL TERMURAH DI DUNIA")
+
+print("\nDAFTAR MEREK MOBIL")
+print("1. AVANZA - Rp 300.000/hari")
+print("2. CIVIC - Rp 500.000/hari")
+print("3. TOYOTA - Rp 400.000/hari")
+print("4. KELUAR")
+
+pilihan = input("Pilih opsi (1-4): ")
+
+if pilihan == '1':
+    print("Pilihan warna untuk AVANZA:")
+    print("1. Merah")
+    print("2. Putih")
+    print("3. Hitam")
+    warna = input("Pilih warna (1-3): ")
+    jam = int(input("Berapa jam sewa? "))
+    total_harga = 300000 * (jam / 24)
+    print(f"Harga sewa AVANZA selama {jam} jam: Rp {total_harga:.2f}")
+elif pilihan == '2':
+    print("Pilihan warna untuk CIVIC:")
+    print("1. Biru")
+    print("2. Hitam")
+    print("3. Putih")
+    warna = input("Pilih warna (1-3): ")
+    jam = int(input("Berapa jam sewa? "))
+    total_harga = 500000 * (jam / 24)
+    print(f"Harga sewa CIVIC selama {jam} jam: Rp {total_harga:.2f}")
+elif pilihan == '3':
+    print("Pilihan warna untuk TOYOTA:")
+    print("1. Silver")
+    print("2. Hitam")
+    print("3. Merah")
+    warna = input("Pilih warna (1-3): ")
+    jam = int(input("Berapa jam sewa? "))
+    total_harga = 400000 * (jam / 24)
+    print(f"Harga sewa TOYOTA selama {jam} jam: Rp {total_harga:.2f}")
+elif pilihan == '4':
+    print("Terima kasih, sampai jumpa!")
+else:
+    print("Opsi tidak valid. Silakan coba lagi.")
---
+++ README.md
@@ -0,0 +1,13 @@
+# JUDUL
+PROGRAM UNTUK MEMBANTU TRANSAKSI DI RENTAL MOBIL
+
+==================================================
+
+# DESKIPSI PROGRAM
+
+Program ini membantu untuk mengelola sistem rental mobil. Dengan menggunakan Python, program ini menyediakan fitur-fitur utama yang memudahkan pengguna dalam menyewa mobil dan dan mempermudah transaksi penyewaannya
+
+---
+
+# FlowChart
+!(FlowChart)[]
